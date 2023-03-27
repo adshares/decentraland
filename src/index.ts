ef29738533380e6c55f1e94854bcd6f93e8399fc
Maciej Pilarczyk <m.pilarczyk@outlook.com>
2023-03-27 13:24:11 +0200

    Stands (#8)
    * Totem
    
    * fix placement position
    
    * add billboards
    
    * add rotating citylight
    
    * readme
    
    * readme
    
    * fix review remarks
    
    * models with colliders
    
    * Refactor build script - rollup support
    
    ---------
    
    Co-authored-by: MykolaZhura <nikoyea14@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import Creative from './creative'
 import { Chain } from './enums'
-import { PlainPlacement } from './placement'
+import { IPlacement, PlainPlacement } from './placement'
+import { IStand, Totem, Billboard, Citylight } from './stand'
 import SupplyAgent from './supplyAgent'
 
 export {
   Chain,
   SupplyAgent,
+  IPlacement,
   PlainPlacement,
+  IStand,
   Creative,
+  Totem,
+  Billboard,
+  Citylight
 }
