0728bdfc09e17cd32f2cdcc47be39511251ad5fc
Niko Yea <nikoyea14@gmail.com>
2023-04-06 00:54:31 +0200

    ui placement set interval

--- src/timer.ts
+++ src/timer.ts
@@ -1,13 +1,14 @@
 type ITimerComponent = {
+  id?: string
   elapsedTime: number
   targetTime: number
-  onTargetTimeReached: () => void
+  onTargetTimeReached: (components: ITimerComponent[], index) => void
 }
 
-class TimerSystem implements ISystem {
+export class TimerSystem implements ISystem {
   private static _instance: TimerSystem | null = null
 
-  private _components: ITimerComponent[] = []
+  protected _components: ITimerComponent[] = []
 
   static createAndAddToEngine (): TimerSystem {
     if (this._instance == null) {
@@ -25,25 +26,48 @@ class TimerSystem implements ISystem {
     TimerSystem._instance = this
   }
 
+  clear(id: string) {
+    this._components = this._components.filter(component => {
+      if(!component.id){
+        return true
+      }
+      return component.id.indexOf(id)
+    })
+  }
+
   update (dt: number) {
     this._components.forEach((component, index) => {
       component.elapsedTime += dt
       if (component.elapsedTime >= component.targetTime) {
-        component.onTargetTimeReached()
+        component.onTargetTimeReached(this._components, index)
         this._components.splice(index, 1)
       }
+
     })
   }
 }
 
-export default function setTimeout (fn: () => void, msecs: number) {
+export function setTimeout (fn: () => void, msecs: number) {
   let instance = TimerSystem.createAndAddToEngine()
-
   let timer: ITimerComponent = {
     elapsedTime: 0,
     targetTime: msecs / 1000,
     onTargetTimeReached: fn
   }
-
   instance.addComponent(timer)
+  return instance
+}
+
+export function setInterval (id: string, fn: () => void, msecs: number): TimerSystem {
+  let instance = TimerSystem.createAndAddToEngine()
+  for (let tik = 1000; tik <= msecs; tik += 1000) {
+    let timer: ITimerComponent = {
+      id: id + tik.toString(),
+      elapsedTime: 0,
+      targetTime: tik / 1000,
+      onTargetTimeReached: fn
+    }
+    instance.addComponent(timer)
+  }
+  return instance
 }
